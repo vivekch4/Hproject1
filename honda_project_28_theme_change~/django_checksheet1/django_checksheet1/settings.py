1c9f29a25700f916b9a4462071a1f828ce8e8eb2
workspacetxt <workspace00018@gmail.com>
2025-05-10 16:44:02 +0530

    aaaa

--- honda_project_28_theme_change~/django_checksheet1/django_checksheet1/settings.py
+++ honda_project_28_theme_change~/django_checksheet1/django_checksheet1/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = "_n$o7+1gt*0-bk0^k0s^(!m=_dz64w4ghs@-sv5q3ybi1g@*t1"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ["192.168.1.60"]
-# ALLOWED_HOSTS = ["worksp.pythonanywhere.com"]
+ALLOWED_HOSTS = ["192.168.1.60","122.180.247.129"]
 # Application definition
 
 INSTALLED_APPS = [
