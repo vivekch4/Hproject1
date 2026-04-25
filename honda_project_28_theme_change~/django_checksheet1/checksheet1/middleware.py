45f51b038853082081b1b56373140764cd95b91a
abhayrathor12 <rathorabhay633@gmail.com>
2026-04-25 13:51:00 +0530

    middleware updated

--- honda_project_28_theme_change~/django_checksheet1/checksheet1/middleware.py
+++ honda_project_28_theme_change~/django_checksheet1/checksheet1/middleware.py
@@ -2,11 +2,12 @@ def content_security_policy(get_response):
     def middleware(request):
         response = get_response(request)
         response['Content-Security-Policy'] = (
-    "default-src 'self'; "
-    "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; "
-    "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; "
-    "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; "
-    "img-src 'self' data:; "
-)
+            "default-src 'self'; "
+            "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; "
+            "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; "
+            "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; "
+            "img-src 'self' data:; "
+            "frame-src 'self' https://drive.google.com; "   # ✅ ADD THIS
+        )
         return response
     return middleware
